5e50e4b891ef5e9b13b8079e22f3c228065e84f6
obuloup-stg <obuloup-stg@var16154.cg83.fr>
2020-02-10 15:23:15 +0100

    Probléme ajout selection tout les PCs de la Salle

--- StatConnexion/StatConnexionGUI.py
+++ StatConnexion/StatConnexionGUI.py
@@ -53,39 +53,49 @@ class StatConnexion(QtWidgets.QMainWindow):
         
         
     def statConnexionFilePushButtonPressed(self):
+        #Supression des ellements dans la combobox
         self.salleComboBox.clear()
         
+        #Récuperation du fichier a l'aide du widget QFileDialogue
         cheminTemp = QFileDialog.getOpenFileName(self, 'Choisir College', '', '*.csv')
         self.chemin = cheminTemp[0]
         self.statConnexionFileLineEdit.setText(self.chemin)
         
         listeSalle = []
         numSalle = []
-        
+        numSalle.append("...")        
         if self.chemin.strip() != "":
-        
+            
+            #Lecture du fichier CSV
             with open(self.chemin, newline='') as csvfile:
                 spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
         
                 next(spamreader, None)
         
+                #Remplissage de la combobox PC
                 for row in spamreader:
                     infoPC = row[0]
                     listeSalle.append(infoPC.split("-"))
-        
+                    
+                #Remplissage de la combobox Salle
                 for row in listeSalle:
                     numeroSalle = row[1]
                     numSalle.append(numeroSalle)
             
+            #Supression des doublons dans la liste Salle
             numSalle = list(dict.fromkeys(numSalle))
+
             
+            #Ajout de la liste salle dans la combobox
             self.salleComboBox.addItems(numSalle)
-        
+       
+    #Création d'une fonction pour l'actualisation de la combobox des PC    
     def updatePC(self):
         self.pcComboBox.clear()
         
         Salle = self.salleComboBox.currentText()
         listePC = []
+        listePC.append("...")
         
         if self.chemin.strip() != "":
         
@@ -100,6 +110,7 @@ class StatConnexion(QtWidgets.QMainWindow):
                         listePC.append(infoPC)
         
             listePC = list(dict.fromkeys(listePC))
+
             
             self.pcComboBox.addItems(listePC)
     
@@ -128,7 +139,6 @@ class StatConnexion(QtWidgets.QMainWindow):
                 self.rowListe.append('<link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">')
                 self.rowListe.append('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>')
                 self.rowListe.append('<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>')
-              
                 self.rowListe.append("</head>")
                 self.rowListe.append("<body>")
                 self.rowListe.append('<table id="resultats"><thead><tr><th>Machine Id</th><th>Etat</th><th>Utilisateur</th><th>Date</th><th>Heure</th></tr></thead><tbody>')
@@ -137,11 +147,8 @@ class StatConnexion(QtWidgets.QMainWindow):
                     date = row[3]
                     pc = row[0]
                     
-                    if dateMinimum <= date and dateMaximum >=  date and pc.split("-")[1] == Salle:
-                        self.rowListe.append("<tr><td>" + row[0] + "</td>" + "<td>" + row[1] + "</td>" + "<td>" + row[2] + "</td>"+ "<td>" + row[3] + "</td>"+ "<td>" + row[4] + "</td>"+ "</tr>")
                     #PC de la salle
                     if dateMinimum <= date and dateMaximum >=  date and pc == PC and row[1] == "1":
-                        #print(row)
                         connexion = connexion + 1
                         
                     #Salle
@@ -154,6 +161,16 @@ class StatConnexion(QtWidgets.QMainWindow):
                         connexionTotale = connexionTotale + 1
                         PCCollege.append(row[0])
                         
+                    if dateMinimum <= date and dateMaximum >=  date and pc == PC:     
+                        self.rowListe.append("<tr><td>" + row[0] + "</td>" + "<td>" + row[1] + "</td>" + "<td>" + row[2] + "</td>"+ "<td>" + row[3] + "</td>"+ "<td>" + row[4] + "</td>"+ "</tr>")                   
+                    
+                    if dateMinimum <= date and dateMaximum >= date and pc.split("-")[1] == Salle and PC == "...":
+                        self.rowListe.append("<tr><td>" + row[0] + "</td>" + "<td>" + row[1] + "</td>" + "<td>" + row[2] + "</td>"+ "<td>" + row[3] + "</td>"+ "<td>" + row[4] + "</td>"+ "</tr>")
+            
+                    if dateMinimum <= date and dateMaximum >= date and Salle == "...":
+                        self.rowListe.append("<tr><td>" + row[0] + "</td>" + "<td>" + row[1] + "</td>" + "<td>" + row[2] + "</td>"+ "<td>" + row[3] + "</td>"+ "<td>" + row[4] + "</td>"+ "</tr>")
+                
+                
                 PCCollege = list(dict.fromkeys(PCCollege))
                 nombrePCUtilisie = len(PCCollege)
                 
@@ -168,9 +185,11 @@ class StatConnexion(QtWidgets.QMainWindow):
                 self.rowListe.append("} );")
                 self.rowListe.append("</script>")
                 self.rowListe.append("</html>")
-
+                
             
             self.bilan(connexion, dateMinimum, dateMaximum, PC, Salle, connexionTotale, connexionTotaleSalle, nombrePC, nombrePCUtilisie)
+            
+            
         
     def showCalendarDateDebut(self):
         self.calendar = calendarGUI.Calendar()
@@ -201,12 +220,16 @@ class StatConnexion(QtWidgets.QMainWindow):
         self.ui.dateDebutLineEdit.setText(dateDebut)
             
     def bilan(self, connexion, dateMinimum, dateMaximum, PC, Salle, connexionTotale, connexionTotaleSalle, nombrePC, nombrePCUtilisie):
+        print("ok")
+        
+        #convertion des variables int en str
         connexion = str(connexion)
         connexionTotale = str(connexionTotale)
         connexionTotaleSalle = str(connexionTotaleSalle)
         nombrePC = str(nombrePC)
         nombrePCUtilisie = str(nombrePCUtilisie)
         
+        #Affichage des resultat
         self.ui.resultatTextEdit.append("*----------------Infos College---------------*")
         self.ui.resultatTextEdit.append("Du : "+dateMinimum+" au : "+dateMaximum)
         self.ui.resultatTextEdit.append("Nombre De Connexion: "+connexionTotale)
@@ -222,23 +245,23 @@ class StatConnexion(QtWidgets.QMainWindow):
           
         self.ui.resultatTextEdit.append("")
         
-        self.ui.resultatTextEdit.append("*------------------Infos PC------------------*")  
+        self.ui.resultatTextEdit.append("*------------------Infos PC------------------*")
+        
         self.ui.resultatTextEdit.append("PC : "+PC)
         self.ui.resultatTextEdit.append("Du : "+dateMinimum+" au : "+dateMaximum)
         self.ui.resultatTextEdit.append("Nombre De Connexion: "+connexion)
       
     def selectionFichier(self):
+        #Affichage de la boite dialogue pour la selection du chemin d'export
         self.cheminExportTemp = QFileDialog.getOpenFileName(self, 'Choisire Destination', '', '*.html')
         self.cheminExport = self.cheminExportTemp[0]
         self.fichierEnregstrerLineEdit.setText(self.cheminExport)
   
     def exporter(self):
 
+        #Ecriture du text html a l'interieur du document selectionner
         self.Export = open(self.cheminExport, "w")
         
         for row in self.rowListe:
             self.Export.write(row)
-        
         self.Export.close()           
-                
-        
